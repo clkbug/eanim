5409a7fae8dfd5e342aa240d6cefc3a432f44bf3
clkbug <mail@clkbug.net>
2023-06-08 21:56:30 +0900

    適当にアニメーションするのを追加

--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	_ "image/png"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type scene struct {
+	t             int
+	idx           int
+	framePerImage int
+	img           []*ebiten.Image
+	width, height int
+}
+
+func (s *scene) Update() error {
+	s.t++
+	if s.t%s.framePerImage == 0 {
+		s.idx++
+		s.idx = (s.idx + 1) % len(s.img)
+	}
+	return nil
+}
+
+func (s *scene) Draw(screen *ebiten.Image) {
+	screen.DrawImage(s.img[s.idx], &ebiten.DrawImageOptions{})
+}
+
+func (s *scene) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return s.width, s.height
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
 
 func main() {
-	fmt.Println("hello")
+	scene := &scene{
+		framePerImage: 10,
+		width:         1,
+		height:        1,
+	}
+	for _, arg := range os.Args[1:] {
+		fp, err := os.Open(arg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		img, _, err := image.Decode(fp)
+		if err != nil {
+			log.Fatal(err)
+		}
+		eimg := ebiten.NewImageFromImage(img)
+		scene.img = append(scene.img, eimg)
+		scene.width = max(scene.width, eimg.Bounds().Dx())
+		scene.height = max(scene.height, eimg.Bounds().Dy())
+	}
+	ebiten.SetWindowSize(scene.width, scene.height)
+	ebiten.SetWindowTitle("eanim")
+
+	if err := ebiten.RunGame(scene); err != nil {
+		log.Fatal(err)
+	}
 }
